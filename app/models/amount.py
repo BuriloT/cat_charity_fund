854daed2baeeb39e48e2d59e3df3ee9d04147133
Илья Борецкий <boretskiy38@gmail.com>
2022-09-24 04:05:42 +0300

    Add abstrct amount model

--- app/models/amount.py
+++ app/models/amount.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, Boolean, DateTime
 
 class Amount:
     __abstract__ = True
-    
+
     full_amount = Column(Integer)
     invested_amount = Column(Integer, default=0)
     fully_invested = Column(Boolean, default=False)
